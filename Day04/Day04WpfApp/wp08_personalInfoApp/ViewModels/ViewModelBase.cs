d3aefe71697e795cf102c088506cf9f73a88d3f9
jangsihyeon <42333610jsh@gmail.com>
2023-04-21 15:43:07 +0900

    4월 21일
    오늘은 정처기 실기땜시 일찍~

---
+++ Day04/Day04WpfApp/wp08_personalInfoApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wp08_personalInfoApp.ViewModels
+{
+    // 프로퍼티가 변경 되면 시스템에 알려주는 역할 
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void RaisePropertyChanged(string propertyNames)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyNames));
+            }
+        }
+    }
+}
